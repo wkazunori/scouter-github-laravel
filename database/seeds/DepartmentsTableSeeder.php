08fd6b09d4f93b34c33b6e302756a87ab9eafc5c
wkazunori <wakamatsu.kazunori@gmail.com>
2021-03-30 14:23:42 +0900

    update

---
+++ database/seeds/DepartmentsTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DepartmentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('departments')->insert([
+            'name' => '未選択',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('departments')->insert([
+            'name' => '営業部',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('departments')->insert([
+            'name' => '製造部',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('departments')->insert([
+            'name' => '経理部',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('departments')->insert([
+            'name' => '総務部',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('departments')->insert([
+            'name' => '広報部',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('departments')->insert([
+            'name' => '人事部',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+    }
+}
